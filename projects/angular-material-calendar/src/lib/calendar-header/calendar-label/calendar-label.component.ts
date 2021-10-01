70391acc8741f172fdcc903b115dfcea54cae60f
vaibhav <vaibhav@upbytes.in>
2021-10-01 20:17:57 +0530

    AC-6 create calendar header compoenent

---
+++ projects/angular-material-calendar/src/lib/calendar-header/calendar-label/calendar-label.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { CalendarDate } from '../../calendar-date/calendar-date';
+import { DateService } from '../../service/date.service';
+
+@Component({
+    selector: 'angular-material-calendar-label',
+    templateUrl: './calendar-label.component.html',
+  })
+export class CalendarHeaderLabelComponent {
+    date$?: Observable<CalendarDate>;
+    _currentDate?: CalendarDate;
+  
+    constructor(private store: Store<{ _date: CalendarDate}>,
+                private _dateService: DateService) {
+      this.date$ = store.select('_date');
+      this.date$.subscribe((d: CalendarDate) => this._currentDate = d);
+    }
+
+    get month(): string {
+        return this._dateService.getMonthNames(this._currentDate!, 'long');
+    }
+  
+    get year(): string {
+        return this._dateService.getYearName(this._currentDate!, 'long');
+    }
+
+    get label(): string {
+        return `${this.month} ${this.year}`;
+    }
+
+}
