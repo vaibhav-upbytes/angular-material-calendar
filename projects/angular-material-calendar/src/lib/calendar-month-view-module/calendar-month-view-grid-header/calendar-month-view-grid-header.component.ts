22d292f7f99f827b5aba6a631327149b78b0572d
vaibhav <vaibhav@upbytes.in>
2021-10-03 09:01:43 +0530

    Ac-13 create month view header component

---
+++ projects/angular-material-calendar/src/lib/calendar-month-view-module/calendar-month-view-grid-header/calendar-month-view-grid-header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MonthViewService } from '../service/calendar-month-view.service';
+
+@Component({
+  selector: 'angular-material-calendar-month-view-grid-header',
+  templateUrl: './calendar-month-view-grid-header.component.html',
+  styleUrls: [
+    './calendar-month-view-grid-header.component.scss'
+  ]
+})
+export class CalendarMonthViewGridHeaderComponent {
+    dayOfWeekNames?: string[];
+
+    constructor(
+        private _monthViewService: MonthViewService, 
+    ) {
+        this.dayOfWeekNames = this._monthViewService.getDayOfWeeksNames('short');
+    }
+
+}
