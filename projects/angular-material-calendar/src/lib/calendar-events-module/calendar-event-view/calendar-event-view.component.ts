8fc6eb3d2ad75b110e8ed7a3463412f7c4487c8c
vaibhav <vaibhav@upbytes.in>
2021-10-23 18:37:07 +0530

    AC-92 change month view event view implementation

--- projects/angular-material-calendar/src/lib/calendar-events-module/calendar-event-view/calendar-event-view.component.ts
+++ projects/angular-material-calendar/src/lib/calendar-events-module/calendar-event-view/calendar-event-view.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { CalendarEventFull } from '../../calendar-modal/calendar-event/calendar-event-full';
 
 import { CalendarEventInput } from '../../calendar-modal/calendar-event/calendar-event-input';
 import { MonthViewService } from '../../calendar-month-view-module/service/calendar-month-view.service';
+import { CalendarEventService } from '../../service/calendar-event.service';
 
 @Component({
   selector: 'angular-material-calendar-event-view',
@@ -12,9 +14,16 @@ import { MonthViewService } from '../../calendar-month-view-module/service/calen
 })
 export class CalendarEventViewComponent {
     @Input() events?: CalendarEventInput[];
-    constructor(private monthViewService: MonthViewService) {}
+    time?: string;
+
+    constructor(private monthViewService: MonthViewService,
+      private calendarEventService: CalendarEventService) { }
 
     getDisplay(tile: CalendarEventInput): string {
       return this.monthViewService.getEventDisplay(tile);
+    }
+
+    getTitle(e: CalendarEventFull): string {
+      return this.time = this.calendarEventService.eventsubtitle(e);
     }
 }
