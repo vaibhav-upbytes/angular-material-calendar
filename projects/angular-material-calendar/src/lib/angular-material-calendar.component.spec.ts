1f51e1a5d1a302442d242b4e3eb4187a1e980e53
vaibhav <vaibhav@upbytes.in>
2021-10-29 17:46:59 +0530

    AC-150 implement data source for generic data type

--- projects/angular-material-calendar/src/lib/angular-material-calendar.component.spec.ts
+++ projects/angular-material-calendar/src/lib/angular-material-calendar.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AngularMaterialCalendarComponent } from './angular-material-calendar.component';
+import { CalendarEventInput } from './calendar-modal/calendar-event/calendar-event-input';
 
 describe('AngularMaterialCalendarComponent', () => {
-  let component: AngularMaterialCalendarComponent;
-  let fixture: ComponentFixture<AngularMaterialCalendarComponent>;
+  let component: AngularMaterialCalendarComponent<CalendarEventInput>;
+  let fixture: ComponentFixture<AngularMaterialCalendarComponent<CalendarEventInput>>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
