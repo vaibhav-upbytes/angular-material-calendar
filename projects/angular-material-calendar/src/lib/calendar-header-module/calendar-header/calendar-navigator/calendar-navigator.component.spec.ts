54f5843473502d566904411d43f6601d98c1b9e3
vaibhav <vaibhav@upbytes.in>
2021-11-06 17:24:13 +0530

    AC-182 add test cases and api docs for calendar navigator component

---
+++ projects/angular-material-calendar/src/lib/calendar-header-module/calendar-header/calendar-navigator/calendar-navigator.component.spec.ts
@@ -0,0 +1,62 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LuxonDateAdapter, MAT_LUXON_DATE_ADAPTER_OPTIONS } from '@angular/material-luxon-adapter';
+import { DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
+import { StoreModule } from '@ngrx/store';
+import { CALENDAR_REDUCER_TOKEN } from '../../../angular-material-calendar.module';
+import { CalendarNavigatorComponent } from './calendar-navigator.component';
+import { MaterialModule } from '../../../material-modules/material.module';
+import { DateTime } from 'luxon';
+
+let loader: HarnessLoader;
+let component: CalendarNavigatorComponent;
+let fixture: ComponentFixture<CalendarNavigatorComponent>;
+
+describe('calendar-navigator-component', () => {
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                StoreModule.forRoot(CALENDAR_REDUCER_TOKEN),
+                MaterialModule
+            ],
+            declarations: [CalendarNavigatorComponent],
+            providers: [
+                {
+                    provide: DateAdapter,
+                    useClass: LuxonDateAdapter,
+                    deps: [MAT_DATE_LOCALE, MAT_LUXON_DATE_ADAPTER_OPTIONS]
+                }
+            ]
+
+        }).compileComponents();
+        fixture = TestBed.createComponent(CalendarNavigatorComponent);
+        component = fixture.componentInstance;
+        loader = TestbedHarnessEnvironment.loader(fixture);
+
+    });
+
+    it('should work', async () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('Previous month should be displayed', async () => {
+        const previous = await loader.getHarness(MatButtonHarness.with({ selector: '#calendar-navigator-previous' }));
+        await previous.click();
+        expect(fixture.componentInstance._currentDate!.current.month).toEqual(DateTime.now().minus({ months : 1}).month);
+    });
+
+    it('Next month should be displayed', async () => {
+        const next = await loader.getHarness(MatButtonHarness.with({ selector: '#calendar-navigator-next' }));
+        await next.click();
+        expect(fixture.componentInstance._currentDate!.current.month).toEqual(DateTime.now().plus({ months : 1}).month);
+    });
+
+    it('Current month should be displayed', async () => {
+        const today = await loader.getHarness(MatButtonHarness.with({ selector: '#calendar-navigator-today' }));
+        await today.click();
+        expect(fixture.componentInstance._currentDate!.current.month).toEqual(DateTime.now().month);
+    });
+})
+
