423df77a9f4684dcb2780dc2cc2fbeec7c829b5d
vaibhav <vaibhav@upbytes.in>
2021-11-08 20:04:25 +0530

    AC-200 add test cases and api docs for calendar event dialoge

--- projects/angular-material-calendar/src/lib/calendar-events-module/calendar-event-full-view/calendar-event-full-view.component.spec.ts
+++ projects/angular-material-calendar/src/lib/calendar-events-module/calendar-event-full-view/calendar-event-full-view.component.spec.ts
@@ -1,6 +1,7 @@
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { MatCardHarness } from '@angular/material/card/testing';
+import { MatDialogHarness } from '@angular/material/dialog/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LuxonDateAdapter, MAT_LUXON_DATE_ADAPTER_OPTIONS } from '@angular/material-luxon-adapter';
 import { DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
@@ -8,15 +9,17 @@ import { StoreModule } from '@ngrx/store';
 import { CALENDAR_REDUCER_TOKEN } from '../../angular-material-calendar.module';
 import { MaterialModule } from '../../material-modules/material.module';
 import { CalendarEventFullViewComponent } from './calendar-event-full-view.component';
+import { By } from '@angular/platform-browser';
 
 let loader: HarnessLoader;
+let rootLoader: HarnessLoader;
 let component: CalendarEventFullViewComponent;
 let fixture: ComponentFixture<CalendarEventFullViewComponent>;
 
-export const event =     {
+export const event = {
     "start": 1634621592000,
     "end": 1634625192000,
-    "title": "string  string string string string string" ,
+    "title": "string  string string string string string",
     "color": "#e2e6c7"
 };
 
@@ -41,6 +44,7 @@ describe('calendar-event-full-view', () => {
         component = fixture.componentInstance;
         component.event = event;
         fixture.detectChanges();
+        rootLoader = TestbedHarnessEnvironment.documentRootLoader(fixture);
         loader = TestbedHarnessEnvironment.loader(fixture);
 
     });
@@ -59,6 +63,16 @@ describe('calendar-event-full-view', () => {
         expect(cards.length).toBe(1);
         expect(await cards[0].getSubtitleText()).toBe(component.time!);
         expect(await cards[0].getText()).toBe(`${component.time}${component.event?.title!}`);
+    });
+
+    it('should load harness for dialog', async () => {
+        fixture.componentInstance.openEventDialog();
+        fixture.detectChanges();
+        const dialogs = await rootLoader.getHarness(MatDialogHarness);
+        expect(dialogs).toBeTruthy();
+        dialogs.close();
+        fixture.detectChanges();
+        expect(dialogs).toBeTruthy();      
     });
 })
 
